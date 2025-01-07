81d66f928d6f57342b103dbe3ef6ee1c6d662235
bhomaleprajwal19 <bhomaleprajwal19@gmail.com>
2025-01-07 20:18:14 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-   <nav className="flex justify-between bg-[#9b2bf7] text-white py-3">
-    <div className="logo">
-      <span className="font-bold text-xl mx-9"> myTask</span>
-    </div>
-    <ul className=" flex gap-8 mx-9">
-      <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-      <li className='cursor-pointer hover:font-bold transition-all'>Your Task</li>
-    </ul>
-   </nav>
+    <nav className='flex justify-around bg-indigo-900 text-white py-2'>
+        <div className="logo">
+            <span className='font-bold text-xl mx-8'>myTask</span>
+        </div>
+      <ul className="flex gap-8 mx-9">
+        <li className='cursor-pointer hover:font-bold transition-all'> Home </li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+      </ul>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar;
